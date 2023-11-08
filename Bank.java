bd8a276a8eeb556c176b8929e03fee5a275b9bff
clortega <carlos.l.ortega@outlook.com>
2023-11-08 15:47:35 -0800

    updates

--- Bank.java
+++ Bank.java
@@ -1,27 +1,67 @@
 /**
  * Bank
  */
-enum AccountType {SAVINGS, CHECKING}
+enum AccountTypeEnum {SAVINGS, CHECKING}
 
 public class Bank {
+    final String RoutingNumber = "123456";
 
-private float Balance;     
-    
-public float getBalance() 
+    //This balance will track the overall balance of the bank
+    private static double BankBalance;
+
+    private double Balance; 
+    AccountTypeEnum AccountType;
+    String AuthorizedUser;
+
+public Bank() {
+    AccountType = AccountTypeEnum.CHECKING;
+    AuthorizedUser= "";
+}
+
+public Bank(String incAuthorizedUser) {
+    this.AuthorizedUser = incAuthorizedUser;
+    AccountType = AccountTypeEnum.CHECKING;}
+
+public Bank(String incAuthorizedUser, double balance) {
+    this.AuthorizedUser = incAuthorizedUser;
+    AccountType = AccountTypeEnum.CHECKING;
+    BankBalance += balance;
+    Balance = balance;
+}
+
+public double getBalance() 
 {
     return Balance;
 }
-
-public float Deposit(float deposit)
+public double getBankBalance() {
+    return BankBalance;
+}
+public double Deposit(double amount)
 {
-    Balance += deposit;
+    BankBalance += amount;
     return Balance;
 }
-public float Withdraw(float withdraw)
-{
-    Balance -= withdraw;
-    return Balance;
+public double Withdraw(double amount)
+{   double updatedBalance = Balance - amount;
+    double rtnBalance = 0;
+    if(updatedBalance >= 0){
+        Balance -= amount;
+        BankBalance -= amount;
+        rtnBalance = updatedBalance;
+
+    } else {
+        double zeroBalance = 0;
+        Balance = zeroBalance;
+        BankBalance -=amount;
+        rtnBalance = zeroBalance;
+    }
+    BankBalance -= amount;
+
+    //System.out.println(zeroBalance);
+    return rtnBalance;
 }
+    public static double ConvertUSDtoAUD(double usd) {
+        return usd * 1.56;
 
-
+    }
 }
--- Main.java
+++ Main.java
@@ -1,20 +1,68 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Bank bank1 = new Bank(); //the 'new' keyword, lets us know we are creating a new instance of bank
-        bank1.Deposit(500.0f);
-        bank1.Deposit(50.0f);
-        bank1.Deposit(10.0f);
-        float currentBalance;
-        currentBalance = bank1.getBalance();
-        System.out.println(currentBalance);
+        Bank bank1 = new Bank("Melissa", 900); //the 'new' keyword, lets us know we are creating a new instance of bank
+        System.out.println(bank1.getBalance());
+        System.out.println(bank1.getBankBalance());
 
-        bank1.Withdraw(50.0f);
-        currentBalance = bank1.getBalance();
-        System.out.println(currentBalance);
+        Bank bank2 = new Bank("Eric", 500);
+        System.out.println(bank2.getBalance());
+        System.out.println(bank2.getBankBalance());
+
+        Bank bank3 = new Bank("Peter", 1000);
+        System.out.println(bank3.getBalance());
+        System.out.println(bank3.getBankBalance());
 
 
+        // System.out.println(bank1.AuthorizedUser);
+        System.out.println(bank1.getBalance());
+        // System.out.println(bank1.AccountType);
 
+        // bank1.Deposit(500.0);
+        // bank1.Deposit(50.0);
+        // bank1.Deposit(10.0);
+        // double currentBalance;
+        // currentBalance = bank1.getBalance();
+        // System.out.println(currentBalance);
+
+        // bank1.Withdraw(50.0);
+        // currentBalance = bank1.getBalance();
+        // System.out.println(currentBalance);
+
+        // Scanner scanner = new Scanner(System.in);
+        // scanner.nextLine();
+
+        System.out.println(bank2.AuthorizedUser);
+
+        //How to use a switch
+        String name = "Peter";
+        switch (name) {
+            case "M" :
+            System.out.println("name is M");
+                break;
+                case "Peter":
+                System.out.println("Peter is the name");
+                break;
+            default:
+            System.out.println("Name doesn't match");
+                break;
+        }
+        //Switches are like if statements. The above could've been written with an if statement.
+
+        Sqrt();
 
     }
-
+    public static void Sqrt() {
+        Scanner scanner = new Scanner(System.in);
+        try{
+        System.out.println("Enter an integer");
+        int userinput = scanner.nextInt();
+        System.out.println("Square root is " + Math.sqrt(userinput));
+        } catch(InputMismatchException e) {
+            System.out.println("Please enter integers");
+        }
+        scanner.close();
+    }
 }
